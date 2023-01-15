6f17ed89d775852dd417c682b7eefbc3423e046a
Matheus Oliveira <45184376+matheusolivv@users.noreply.github.com>
2023-01-15 01:10:53 -0300

    xx

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
